4c756d32faa9aae87f0d46927f4b204377d21650
SamHoney71 <gjeter91@gmail.com>
2021-01-24 21:01:26 -0600

    Popup Text is correct

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -15,9 +15,9 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: API_KEY
 }).addTo(myMap);
  
-//Store API query variables with 1000 limit 
+//Store API query variables with 10000 limit 
 var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson?";
-var limit = "&$limit=100";
+var limit = "&$limit=10000";
 
 //Assemble API query URL
 var url = baseURL + limit;
@@ -51,10 +51,18 @@ d3.json(url, function(data) {
         // var depth = earthquake_loc.coordinates[2];
         // // console.log(depth);
 
+        // variables for popups
+        var earthquake_prop = features[i].properties;
+        // console.log(earthquake_prop);
+        var magnitude = earthquake_prop.mag;
+        // console.log(magnitude)
+        var place = earthquake_prop.place;
+        // console.log(place)
+
 
         if (earthquake_loc) {
             markers.addLayer(L.marker([earthquake_loc.coordinates[1], earthquake_loc.coordinates[0]])
-                .bindPopup(earthquake_loc.coordinates[2]));
+                .bindPopup("<h1>Magnitude: " + magnitude +"</h1> <hr> <h3> Location: " + place + "</h3>"));
         }
         
     }
