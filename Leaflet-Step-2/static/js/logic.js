bd012d7fd28544c0404ea75495c18797ca537c70
SamHoney71 <gjeter91@gmail.com>
2021-01-31 07:05:51 -0600

    Moved logic.js to the correct Leaflet folder

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -23,48 +23,73 @@ var limit = "&$limit=10000";
 var url = baseURL + limit;
 console.log(url);
 
-
-d3.json(url, function createMarkers(data) {
-    
+//Grab data with D3
+d3.json(url, function(data) {
+    // console.log(data);
+   
+    // pull out 'features' in Json
     var features = data.features;
+    // console.log(features)
 
+    // Create marker cluster group - need to get rid of clusters
+    var markers = L.markerClusterGroup();
+    // console.log(markers)
 
-    for (var i = 0; i<features.length; i++) {
+    //loop through data
+    for (var i = 0; i< features.length; i++) {
 
+        //Set earthquake location to a variable
         var earthquake_loc = features[i].geometry;
+            
+        // // set variable for lat, long, depth
+        var lng = earthquake_loc.coordinates[0];
+        // // console.log(lng);
+        var lat = earthquake_loc.coordinates[1];
+        // // console.log(lat);
+        var depth = earthquake_loc.coordinates[2];
+        console.log(depth);
+
+        // variables for earthquake properties
+        var earthquake_prop = features[i].properties;
+        // console.log(earthquake_prop);
+        var magnitude = earthquake_prop.mag;
+        // console.log(magnitude)
+        var place = earthquake_prop.place;
+        // console.log(place)
 
         // Conditional for depth colors
         var color = "";
-        if (features[i].geometry.coordinates[2] > 90) {
+        if (earthquake_loc.coordinates[2] > 90) {
             color = "Red";
         }
-        else if (features[i].geometry.coordinates[2] > 70) {
+        else if (earthquake_loc.coordinates[2] > 70) {
             color = "Orange";
         }
-        else if (features[i].geometry.coordinates[2] > 50) {
+        else if (earthquake_loc.coordinates[2] > 50) {
             color = "red";
         }
-        else if (features[i].geometry.coordinates[2] > 30) {
+        else if (earthquake_loc.coordinates[2] > 30) {
             color = "yellow";
         }
         else {
             color = "green;"
         }
 
-        L.circle([earthquake_loc.coordinates[1], earthquake_loc.coordinates[0]], {
-            fillOpacity:0.5,
-            color: "white",
-            fillColor: color,
-            //adjust radius
-            radius: features[i].properties.mag * 50000 
-        })
-        .bindPopup("<h1>Magnitude: " + features[i].properties.mag +"</h1> <hr> <h3> Location: " + features[i].properties.place + "</h3><hr><h3> Depth: "+ features[i].geometry.coordinates[2] + "</h3>")
-        .addTo(myMap);
-    };
-    console.log(earthquake_loc.coordinates[1])
 
+        // Adds markers & pop ups
+        if (earthquake_loc) {
+            markers.addLayer(L.circle([earthquake_loc.coordinates[1], earthquake_loc.coordinates[0]], {
+                fillOpacity:0.25,
+                color: "white",
+                fillColor: color,
+                //adjust radius
+                radius: earthquake_prop.mag * 1500
+            }).bindPopup("<h1>Magnitude: " + magnitude +"</h1> <hr> <h3> Location: " + place + "</h3><hr><h3> Depth: "+ depth + "</h3>"));
+        }
+        
+       
+    }
 
+//Add marker cluster layer to the map
+myMap.addLayer(markers);
 });
-
-
-
