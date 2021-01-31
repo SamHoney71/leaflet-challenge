286634700b774f952ad4cec66089489170dcb5a3
SamHoney71 <gjeter91@gmail.com>
2021-01-31 07:34:15 -0600

    color scheme is correct

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -16,8 +16,8 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(myMap);
  
 //Store API query variables with 10000 limit 
-var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson?";
-var limit = "&$limit=10000";
+var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson?";
+var limit = "&$limit=100000";
 
 //Assemble API query URL
 var url = baseURL + limit;
@@ -36,24 +36,27 @@ d3.json(url, function createMarkers(data) {
         // Conditional for depth colors
         var color = "";
         if (features[i].geometry.coordinates[2] > 90) {
-            color = "Red";
+            color = "rgb(255, 0, 0)";
         }
         else if (features[i].geometry.coordinates[2] > 70) {
-            color = "Orange";
+            color = "rgb(255, 128, 0)";
         }
         else if (features[i].geometry.coordinates[2] > 50) {
-            color = "red";
+            color = "rgb(255, 191, 0)";
         }
         else if (features[i].geometry.coordinates[2] > 30) {
-            color = "yellow";
+            color = "rgb(191, 255,0)";
+        }
+        else if (features[i].geometry.coordinates[2] > 10) {
+            color = "rgb(64, 255, 0)"
         }
         else {
-            color = "green;"
+            color = "rgb(0, 255, 64)"
         }
 
         L.circle([earthquake_loc.coordinates[1], earthquake_loc.coordinates[0]], {
-            fillOpacity:0.5,
-            color: "white",
+            fillOpacity:0.7,
+            color: "",
             fillColor: color,
             //adjust radius
             radius: features[i].properties.mag * 50000 
